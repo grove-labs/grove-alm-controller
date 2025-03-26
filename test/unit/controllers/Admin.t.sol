f5428369f4891f23a20bb6ec491aefbed8ce5e4d
Bartek Rutkowski <barrutko.eth@gmail.com>
2025-03-26 14:41:27 +0100

    chore: remove redundant tests

--- test/unit/controllers/Admin.t.sol
+++ test/unit/controllers/Admin.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.21;
 
-import { ForeignController } from "../../../src/ForeignController.sol";
 import { MainnetController } from "../../../src/MainnetController.sol";
 
 import { MockDaiUsds } from "../mocks/MockDaiUsds.sol";
@@ -79,69 +78,3 @@ contract MainnetControllerAdminTests is UnitTestBase {
     }
 
 }
-
-contract ForeignControllerAdminTests is UnitTestBase {
-
-    event MintRecipientSet(uint32 indexed destinationDomain, bytes32 mintRecipient);
-
-    ForeignController foreignController;
-
-    bytes32 mintRecipient1 = bytes32(uint256(uint160(makeAddr("mintRecipient1"))));
-    bytes32 mintRecipient2 = bytes32(uint256(uint160(makeAddr("mintRecipient2"))));
-
-    function setUp() public {
-        foreignController = new ForeignController(
-            admin,
-            makeAddr("almProxy"),
-            makeAddr("rateLimits"),
-            makeAddr("psm"),
-            makeAddr("usdc"),
-            makeAddr("cctp")
-        );
-    }
-
-    function test_setMintRecipient_unauthorizedAccount() public {
-        vm.expectRevert(abi.encodeWithSignature(
-            "AccessControlUnauthorizedAccount(address,bytes32)",
-            address(this),
-            DEFAULT_ADMIN_ROLE
-        ));
-        foreignController.setMintRecipient(1, mintRecipient1);
-
-        vm.prank(freezer);
-        vm.expectRevert(abi.encodeWithSignature(
-            "AccessControlUnauthorizedAccount(address,bytes32)",
-            freezer,
-            DEFAULT_ADMIN_ROLE
-        ));
-        foreignController.setMintRecipient(1, mintRecipient1);
-    }
-
-    function test_setMintRecipient() public {
-        assertEq(foreignController.mintRecipients(1), bytes32(0));
-        assertEq(foreignController.mintRecipients(2), bytes32(0));
-
-        vm.prank(admin);
-        vm.expectEmit(address(foreignController));
-        emit MintRecipientSet(1, mintRecipient1);
-        foreignController.setMintRecipient(1, mintRecipient1);
-
-        assertEq(foreignController.mintRecipients(1), mintRecipient1);
-
-        vm.prank(admin);
-        vm.expectEmit(address(foreignController));
-        emit MintRecipientSet(2, mintRecipient2);
-        foreignController.setMintRecipient(2, mintRecipient2);
-
-        assertEq(foreignController.mintRecipients(2), mintRecipient2);
-
-        vm.prank(admin);
-        vm.expectEmit(address(foreignController));
-        emit MintRecipientSet(1, mintRecipient2);
-        foreignController.setMintRecipient(1, mintRecipient2);
-
-        assertEq(foreignController.mintRecipients(1), mintRecipient2);
-    }
-
-}
-
