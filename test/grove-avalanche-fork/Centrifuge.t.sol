1963bb4ad8f8f483bf2b5f7b75fb624d7d54e473
Bartek Rutkowski <barrutko.eth@gmail.com>
2025-08-07 13:48:23 +0200

    fix: use explicit imports

--- test/grove-avalanche-fork/Centrifuge.t.sol
+++ test/grove-avalanche-fork/Centrifuge.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity >=0.8.0;
 
-import "../../src/interfaces/CentrifugeInterfaces.sol";
+import { ICentrifugeV3VaultLike, IAsyncRedeemManagerLike, ISpokeLike } from "../../src/interfaces/CentrifugeInterfaces.sol";
 
 import "./ForkTestBase.t.sol";
 
