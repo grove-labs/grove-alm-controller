bfebe93d8b838b8150cfea2b3489f583c755eca3
Bartek Rutkowski <barrutko.eth@gmail.com>
2025-10-18 13:53:11 +0200

    fix: use pyIndexCurrent instead of exchangeRate

--- test/grove-base-fork/Pendle.t.sol
+++ test/grove-base-fork/Pendle.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.21;
 
-import { IPendleMarket, ISY } from "../../src/interfaces/PendleInterfaces.sol";
+import { IPendleMarket, ISY, IYT } from "../../src/interfaces/PendleInterfaces.sol";
 
 import "./ForkTestBase.t.sol";
 
@@ -64,14 +64,14 @@ contract ForeignControllerRedeemFailurePendleTests is PendleTestBase {
     }
 
     function test_redeemPendlePT_rateLimitsBoundary() public {
-        (address sy, address pt,) = pendleMarket.readTokens();
+        (, address pt, address yt) = pendleMarket.readTokens();
         vm.prank(PT_WHALE);
         IERC20(pt).transfer((address(almProxy)), 100_000e18);
 
         vm.warp(pendleMarket.expiry());
 
-        uint256 exchangeRate = ISY(sy).exchangeRate();
-        uint256 exactAmountOut = 50_000e18 * 1e18 / exchangeRate;
+        uint256 pyIndexCurrent = IYT(yt).pyIndexCurrent();
+        uint256 exactAmountOut = 50_000e18 * 1e18 / pyIndexCurrent;
 
         vm.prank(GROVE_EXECUTOR);
         rateLimits.setRateLimitData(redeemKey, exactAmountOut - 1, 1);
@@ -114,14 +114,14 @@ contract ForeignControllerRedeemFailurePendleTests is PendleTestBase {
     }
 
     function test_redeemPendlePT_minAmountOutNotMet() public {
-        (address sy, address pt,) = pendleMarket.readTokens();
+        (, address pt, address yt) = pendleMarket.readTokens();
         vm.prank(PT_WHALE);
         IERC20(pt).transfer((address(almProxy)), 100_000e18);
 
         vm.warp(pendleMarket.expiry());
 
-        uint256 exchangeRate = ISY(sy).exchangeRate();
-        uint256 exactAmountOut = 100_000e18 * 1e18 / exchangeRate; // Exact at this particular point in time
+        uint256 pyIndexCurrent = IYT(yt).pyIndexCurrent();
+        uint256 exactAmountOut = 100_000e18 * 1e18 / pyIndexCurrent; // Exact at this particular point in time
 
         vm.prank(ALM_RELAYER);
         vm.expectRevert("PendleLib/min-amount-not-met");
@@ -141,7 +141,7 @@ contract ForeignControllerRedeemSuccessPendleTests is PendleTestBase {
 
         address ptDonor = PT_WHALE;
 
-        (address sy, address pt,) = pendleMarket.readTokens();
+        (address sy, address pt, address yt) = pendleMarket.readTokens();
         IERC20 yieldToken = IERC20(ISY(sy).yieldToken());
 
         vm.startPrank(ptDonor);
@@ -153,25 +153,25 @@ contract ForeignControllerRedeemSuccessPendleTests is PendleTestBase {
 
         vm.warp(pendleMarket.expiry());
 
-        uint256 exchangeRate   = ISY(sy).exchangeRate();
-        uint256 exactAmountOut = 50_000e18 * 1e18 / exchangeRate;
+        uint256 pyIndexCurrent = IYT(yt).pyIndexCurrent();
+        uint256 exactAmountOut = 50_000e18 * 1e18 / pyIndexCurrent;
 
         vm.prank(ALM_RELAYER);
         foreignController.redeemPendlePT(address(pendleMarket), 50_000e18, exactAmountOut);
 
         assertEq(IERC20(pt).balanceOf(address(almProxy)), 50_000e18);
-        assertEq(IERC20(yieldToken).balanceOf(address(almProxy)), 50_000e18 * 1e18 / exchangeRate);
+        assertEq(IERC20(yieldToken).balanceOf(address(almProxy)), 50_000e18 * 1e18 / pyIndexCurrent);
 
         vm.warp(block.timestamp + 14 days);
 
-        exchangeRate   = ISY(sy).exchangeRate();
-        exactAmountOut = 50_000e18 * 1e18 / exchangeRate;
+        pyIndexCurrent = IYT(yt).pyIndexCurrent();
+        exactAmountOut = 50_000e18 * 1e18 / pyIndexCurrent;
 
         vm.prank(ALM_RELAYER);
         foreignController.redeemPendlePT(address(pendleMarket), 50_000e18, exactAmountOut);
 
         assertEq(IERC20(pt).balanceOf(address(almProxy)), 0);
-        assertEq(IERC20(yieldToken).balanceOf(address(almProxy)), 100_000e18 * 1e18 / exchangeRate);
+        assertEq(IERC20(yieldToken).balanceOf(address(almProxy)), 100_000e18 * 1e18 / pyIndexCurrent);
     }
 
 }
