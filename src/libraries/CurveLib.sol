77f5eb80f41c3eaaf426c1954bbda912bca358f0
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2025-11-13 10:55:49 -0700

    spacing in curvelib

--- src/libraries/CurveLib.sol
+++ src/libraries/CurveLib.sol
@@ -12,8 +12,8 @@ import { RateLimitHelpers } from "../RateLimitHelpers.sol";
 interface ICurvePoolLike is IERC20 {
     function add_liquidity(
         uint256[] memory amounts,
-        uint256 minMintAmount,
-        address receiver
+        uint256          minMintAmount,
+        address          receiver
     ) external;
     function balances(uint256 index) external view returns (uint256);
     function coins(uint256 index) external returns (address);
@@ -27,10 +27,10 @@ interface ICurvePoolLike is IERC20 {
     function get_virtual_price() external view returns (uint256);
     function N_COINS() external view returns (uint256);
     function remove_liquidity(
-        uint256 burnAmount,
+        uint256          burnAmount,
         uint256[] memory minAmounts,
-        address receiver, 
-        bool claimAdminFees
+        address          receiver,
+        bool             claimAdminFees
     ) external;
     function stored_rates() external view returns (uint256[] memory);
 }
