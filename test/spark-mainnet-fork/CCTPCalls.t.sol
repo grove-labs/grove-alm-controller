2c42000cf7e5ffb3285abe1a86e19b1f5839c566
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2025-11-14 11:09:37 -0700

    alignment

--- test/spark-mainnet-fork/CCTPCalls.t.sol
+++ test/spark-mainnet-fork/CCTPCalls.t.sol
@@ -440,20 +440,20 @@ contract USDCToCCTPIntegrationTests is BaseChainUSDCToCCTPTestBase {
     event CCTPTransferInitiated(
         uint32  indexed destinationDomain,
         bytes32 indexed mintRecipient,
-        uint256 usdcAmount
+        uint256         usdcAmount
     );
 
     event DepositForBurn(
         address indexed burnToken,
-        uint256 amount,
+        uint256         amount,
         address indexed depositor,
-        bytes32 mintRecipient,
-        uint32 destinationDomain,
-        bytes32 destinationTokenMessenger,
-        bytes32 destinationCaller,
-        uint256 maxFee,
-        uint32 indexed minFinalityThreshold,
-        bytes hookData
+        bytes32         mintRecipient,
+        uint32          destinationDomain,
+        bytes32         destinationTokenMessenger,
+        bytes32         destinationCaller,
+        uint256         maxFee,
+        uint32 indexed  minFinalityThreshold,
+        bytes           hookData
     );
 
     function test_transferUSDCToCCTP_sourceToDestination() external {
