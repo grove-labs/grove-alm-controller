3d208fd94b4f6eddf121dabcad612294b47b0f36
Bartek Rutkowski <barrutko.eth@gmail.com>
2025-11-17 18:25:52 -0500

    refactor

--- src/ForeignController.sol
+++ src/ForeignController.sol
@@ -15,12 +15,14 @@ import { IERC4626 } from "openzeppelin-contracts/contracts/interfaces/IERC4626.s
 
 import { IPSM3 } from "spark-psm/src/interfaces/IPSM3.sol";
 
-import { IALMProxy }     from "./interfaces/IALMProxy.sol";
-import { ICCTPLike }     from "./interfaces/CCTPInterfaces.sol";
-import { IRateLimits }   from "./interfaces/IRateLimits.sol";
-import { IPendleMarket } from "./interfaces/PendleInterfaces.sol";
+import { IALMProxy }             from "./interfaces/IALMProxy.sol";
+import { ICCTPLike }             from "./interfaces/CCTPInterfaces.sol";
+import { IRateLimits }           from "./interfaces/IRateLimits.sol";
+import { IMerklDistributorLike } from "./interfaces/MerklInterfaces.sol";
+import { IPendleMarket }         from "./interfaces/PendleInterfaces.sol";
 
 import { CurveLib }  from "./libraries/CurveLib.sol";
+import { MerklLib }  from "./libraries/MerklLib.sol";
 import { PendleLib } from "./libraries/PendleLib.sol";
 import { CCTPLib }   from "./libraries/CCTPLib.sol";
 import { ERC20Lib }  from "./libraries/common/ERC20Lib.sol";
@@ -57,6 +59,7 @@ contract ForeignController is AccessControl {
     event MaxSlippageSet(address indexed pool, uint256 maxSlippage);
     event MintRecipientSet(uint32 indexed destinationDomain, bytes32 mintRecipient);
     event RelayerRemoved(address indexed relayer);
+    event MerklDistributorSet(address indexed merklDistributor);
 
     /**********************************************************************************************/
     /*** State variables                                                                        ***/
@@ -91,6 +94,8 @@ contract ForeignController is AccessControl {
     IRateLimits public immutable rateLimits;
     IERC20      public immutable usdc;
     address     public immutable pendleRouter;
+
+    IMerklDistributorLike public merklDistributor;
 
     mapping(address pool                    => uint256 maxSlippage)        public maxSlippages;  // 1e18 precision
     mapping(uint32  destinationDomain       => bytes32 mintRecipient)      public mintRecipients;
@@ -176,6 +181,14 @@ contract ForeignController is AccessControl {
     {
         centrifugeRecipients[destinationCentrifugeId] = recipient;
         emit CentrifugeRecipientSet(destinationCentrifugeId, recipient);
+    }
+
+    function setMerklDistributor(address merklDistributor_)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        merklDistributor = IMerklDistributorLike(merklDistributor_);
+        emit MerklDistributorSet(merklDistributor_);
     }
 
     /**********************************************************************************************/
@@ -667,6 +680,21 @@ contract ForeignController is AccessControl {
             lpBurnAmount       : lpBurnAmount,
             minWithdrawAmounts : minWithdrawAmounts,
             maxSlippage        : maxSlippages[pool]
+        }));
+    }
+
+    /**********************************************************************************************/
+    /*** Relayer Merkl functions                                                                 ***/
+    /**********************************************************************************************/
+
+    function toggleOperatorMerkl(address operator) external {
+        _checkRole(RELAYER);
+        require(address(merklDistributor) != address(0), "ForeignController/merkl-distributor-not-set");
+
+        MerklLib.toggleOperator(MerklLib.MerklToggleOperatorParams({
+            proxy        : proxy,
+            distributor  : merklDistributor,
+            operator     : operator
         }));
     }
 
--- test/grove-base-fork/Merkl.sol
+++ test/grove-base-fork/Merkl.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity >=0.8.0;
 
-import { Ethereum } from "lib/grove-address-registry/src/Ethereum.sol";
+import { Base } from "lib/grove-address-registry/src/Base.sol";
 
 import { IMerklDistributorLike } from "../../src/interfaces/MerklInterfaces.sol";
 
@@ -14,23 +14,40 @@ contract MerklBaseTest is ForkTestBase {
     address operator1 = makeAddr("operator1");
     address operator2 = makeAddr("operator2");
 
-    IMerklDistributorLike merklDistributor = IMerklDistributorLike(Ethereum.MERKL_DISTRIBUTOR);
+    IMerklDistributorLike merklDistributor = IMerklDistributorLike(Base.MERKL_DISTRIBUTOR);
 }
 
-contract MainnetControllerToggleOperatorMerklFailureTests is MerklBaseTest {
+contract ForeignControllerToggleOperatorMerklFailureTests is MerklBaseTest {
+
+    function test_toggleOperatorMerkl_merklDistributorNotSet() external {
+        vm.expectRevert("ForeignController/merkl-distributor-not-set");
+
+        vm.prank(relayer);
+        foreignController.toggleOperatorMerkl(operator1);
+    }
 
     function test_toggleOperatorMerkl_notRelayer() external {
+        vm.prank(Base.GROVE_EXECUTOR);
+        foreignController.setMerklDistributor(address(merklDistributor));
+
         vm.expectRevert(abi.encodeWithSignature(
             "AccessControlUnauthorizedAccount(address,bytes32)",
             address(this),
             RELAYER
         ));
-        mainnetController.toggleOperatorMerkl(operator1);
+        foreignController.toggleOperatorMerkl(operator1);
     }
 
 }
 
-contract MainnetControllerToggleOperatorMerklSuccessTests is MerklBaseTest {
+contract ForeignControllerToggleOperatorMerklSuccessTests is MerklBaseTest {
+
+    function setUp() public override {
+        super.setUp();
+
+        vm.prank(Base.GROVE_EXECUTOR);
+        foreignController.setMerklDistributor(address(merklDistributor));
+    }
 
     function test_toggleOperatorMerkl_singleOperator() external {
         assertEq(merklDistributor.operators(address(almProxy), operator1), 0);
@@ -38,21 +55,21 @@ contract MainnetControllerToggleOperatorMerklSuccessTests is MerklBaseTest {
         vm.prank(relayer);
         vm.expectEmit(address(merklDistributor));
         emit OperatorToggled(address(almProxy), operator1, true);
-        mainnetController.toggleOperatorMerkl(operator1);
+        foreignController.toggleOperatorMerkl(operator1);
 
         assertEq(merklDistributor.operators(address(almProxy), operator1), 1);
 
         vm.prank(relayer);
         vm.expectEmit(address(merklDistributor));
         emit OperatorToggled(address(almProxy), operator1, false);
-        mainnetController.toggleOperatorMerkl(operator1);
+        foreignController.toggleOperatorMerkl(operator1);
 
         assertEq(merklDistributor.operators(address(almProxy), operator1), 0);
 
         vm.prank(relayer);
         vm.expectEmit(address(merklDistributor));
         emit OperatorToggled(address(almProxy), operator1, true);
-        mainnetController.toggleOperatorMerkl(operator1);
+        foreignController.toggleOperatorMerkl(operator1);
 
         assertEq(merklDistributor.operators(address(almProxy), operator1), 1);
     }
@@ -62,25 +79,25 @@ contract MainnetControllerToggleOperatorMerklSuccessTests is MerklBaseTest {
         assertEq(merklDistributor.operators(address(almProxy), operator2), 0);
 
         vm.prank(relayer);
-        mainnetController.toggleOperatorMerkl(operator1);
+        foreignController.toggleOperatorMerkl(operator1);
 
         assertEq(merklDistributor.operators(address(almProxy), operator1), 1);
         assertEq(merklDistributor.operators(address(almProxy), operator2), 0);
 
         vm.prank(relayer);
-        mainnetController.toggleOperatorMerkl(operator1);
+        foreignController.toggleOperatorMerkl(operator1);
 
         assertEq(merklDistributor.operators(address(almProxy), operator1), 0);
         assertEq(merklDistributor.operators(address(almProxy), operator2), 0);
 
         vm.prank(relayer);
-        mainnetController.toggleOperatorMerkl(operator1);
+        foreignController.toggleOperatorMerkl(operator1);
 
         assertEq(merklDistributor.operators(address(almProxy), operator1), 1);
         assertEq(merklDistributor.operators(address(almProxy), operator2), 0);
 
         vm.prank(relayer);
-        mainnetController.toggleOperatorMerkl(operator2);
+        foreignController.toggleOperatorMerkl(operator2);
 
         assertEq(merklDistributor.operators(address(almProxy), operator1), 1);
         assertEq(merklDistributor.operators(address(almProxy), operator2), 1);
