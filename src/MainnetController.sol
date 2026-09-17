3b23127ef8262f9941ae401d68c7c8fc60939006
Bartek Rutkowski <barrutko.eth@gmail.com>
2026-09-17 12:32:03 +0200

    feat: clear DaiUsds migrator approval after swaps

--- src/MainnetController.sol
+++ src/MainnetController.sol
@@ -839,6 +839,8 @@ contract MainnetController is AccessControl {
             address(daiUsds),
             abi.encodeCall(daiUsds.usdsToDai, (address(proxy), usdsAmount))
         );
+
+        ERC20Lib.approve(proxy, address(usds), address(daiUsds), 0);
     }
 
     function swapDAIToUSDS(uint256 daiAmount)
@@ -853,6 +855,8 @@ contract MainnetController is AccessControl {
             address(daiUsds),
             abi.encodeCall(daiUsds.daiToUsds, (address(proxy), daiAmount))
         );
+
+        ERC20Lib.approve(proxy, address(dai), address(daiUsds), 0);
     }
 
     /**********************************************************************************************/
